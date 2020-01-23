8ae51ba24e573e5aee8b1f436bd369d6db8a4b35
bigpixels <gamer21centuries@googlemail.com>
2020-01-23 09:28:35 +0200

    update

--- build/js/index.js
+++ build/js/index.js
@@ -287,41 +287,43 @@ function initGoogleAPI() {
 
 insertGoogleScript();
 
-let vm = new Vue ({
-    el: '#vue',
-    data: {
-        hashtags: [],
-        cities: [],
-        textSearch: '',
-        url: {
-            hashtags: 'https://dka-develop.ru/api?type=hashtag',
-            cities: 'https://dka-develop.ru/api?type=city'
-        }
-    },
-    watch: { //наблюдатель. Следит за свойствами. У него есть список ситуаций, в резултате которых он будет принимать решение. Надо только сказать, за каким свойством ему следить.
-        textSearch: function() { //если значение свойства меняется, срабатывает код, который внутри анонимной функции.
-            if(this.textSearch.length>3) {
-                this.textSearch = 'Меняем содержимое поля из кода';
-            }
-        }
-    },
-    created: function() {
-        console.log(this.url.hashtags); //проверяем, что возвращает свойство. Работает ли в консоли ссылка.
-    },
-    methods: {
-        getHashtags() {
-            //then выполняется, когда мы получили hashtags успешено.
-            axios.get(this.url.hashtags).then((response) => { //такая запись удобна. Можно будет исправить url в data и эта ссылка изменится везде.
-                console.log(this);
-                this.hashtags = response.data;
-            }); 
-        },
-        getCities() {
-            //then выполняется, когда мы получили hashtags успешено.
-            axios.get(this.url.cities).then((response) => { //такая запись удобна. Можно будет исправить url в data и эта ссылка изменится везде.
-                console.log(response.data);
-                this.cities = response.data;
-            }); 
-        }
-    }
-});
+window.addEventListener('load', function () {
+    let vm = new Vue ({
+		el: '#vue',
+		data: {
+			hashtags: [],
+			cities: [],
+			textSearch: '',
+			url: {
+				hashtags: 'https://dka-develop.ru/api?type=hashtag',
+				cities: 'https://dka-develop.ru/api?type=city'
+			}
+		},
+		watch: { //наблюдатель. Следит за свойствами. У него есть список ситуаций, в резултате которых он будет принимать решение. Надо только сказать, за каким свойством ему следить.
+			textSearch: function() { //если значение свойства меняется, срабатывает код, который внутри анонимной функции.
+				if(this.textSearch.length>3) {
+					this.textSearch = 'Меняем содержимое поля из кода';
+				}
+			}
+		},
+		created: function() {
+			console.log(this.url.hashtags); //проверяем, что возвращает свойство. Работает ли в консоли ссылка.
+		},
+		methods: {
+			getHashtags() {
+				//then выполняется, когда мы получили hashtags успешено.
+				axios.get(this.url.hashtags).then((response) => { //такая запись удобна. Можно будет исправить url в data и эта ссылка изменится везде.
+					console.log(this);
+					this.hashtags = response.data;
+				}); 
+			},
+			getCities() {
+				//then выполняется, когда мы получили hashtags успешено.
+				axios.get(this.url.cities).then((response) => { //такая запись удобна. Можно будет исправить url в data и эта ссылка изменится везде.
+					console.log(response.data);
+					this.cities = response.data;
+				}); 
+			}
+		}
+	});	
+})
